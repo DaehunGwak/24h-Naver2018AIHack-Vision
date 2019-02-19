6e324370fee1476bfdf513a1cccfeabf6ecd7aa3
오충협 <tractar0213@gmail.com>
2019-02-19 16:55:40 +0900

    [Create]semi_hard

---
+++ the_final/10_semi_hard/infer.py
@@ -0,0 +1,111 @@
+# -*- coding: utf_8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import nsml
+import numpy as np
+
+from nsml import DATASET_PATH
+import keras
+from keras.models import Model
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics.pairwise import euclidean_distances
+
+
+def bind_model(model):
+    def save(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+        model.save_weights(os.path.join(dir_name, 'model'))
+        print('model saved!')
+
+    def load(file_path):
+        model.load_weights(file_path)
+        print('model loaded!')
+
+    def infer(queries, _):
+        test_path = DATASET_PATH + '/test/test_data'
+
+        db = [os.path.join(test_path, 'reference', path) for path in os.listdir(os.path.join(test_path, 'reference'))]
+
+        queries = [v.split('/')[-1].split('.')[0] for v in queries]
+        db = [v.split('/')[-1].split('.')[0] for v in db]
+        queries.sort()
+        db.sort()
+
+        queries, query_vecs, references, reference_vecs = get_feature(model, queries, db)
+
+        # calculate similarity
+        sim_matrix = np.dot(query_vecs, reference_vecs.T)               # cos_sim
+        indices = np.argsort(sim_matrix, axis=1)
+        indices = np.flip(indices, axis=1)                              # cos_sim
+
+        # query expansion
+        expansion_step = 10
+        m_sample = 5
+        for _ in range(expansion_step):
+            for i, ind in enumerate(indices):
+                query_vecs[i] = np.mean(
+                    np.vstack(
+                        (query_vecs[i], reference_vecs[ind[:m_sample]])
+                    ), axis=0)
+            # recalculate
+            sim_matrix = np.dot(query_vecs, reference_vecs.T)                   # cos_sim
+            indices = np.argsort(sim_matrix, axis=1)
+            indices = np.flip(indices, axis=1)                                  # cos_sim
+
+        retrieval_results = {}
+        for (i, query) in enumerate(queries):
+            ranked_list = [references[k] for k in indices[i]]
+            ranked_list = ranked_list[:1000]
+
+            retrieval_results[query] = ranked_list
+        print('done')
+
+        return list(zip(range(len(retrieval_results)), retrieval_results.items()))
+
+    # DONOTCHANGE: They are reserved for nsml
+    nsml.bind(save=save, load=load, infer=infer)
+
+
+def l2_normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+# data preprocess
+def get_feature(model, queries, db):
+    img_size = (224, 224)
+    test_path = DATASET_PATH + '/test/test_data'
+
+    # intermediate_layer_model = Model(inputs=model.input, outputs=model.output)
+    test_datagen = ImageDataGenerator(rescale=1. / 255, dtype='float32')
+    query_generator = test_datagen.flow_from_directory(
+        directory=test_path,
+        target_size=(224, 224),
+        classes=['query'],
+        color_mode="rgb",
+        batch_size=32,
+        class_mode=None,
+        shuffle=False
+    )
+    query_vecs = model.predict_generator(query_generator, steps=len(query_generator), verbose=1)
+
+    reference_generator = test_datagen.flow_from_directory(
+        directory=test_path,
+        target_size=(224, 224),
+        classes=['reference'],
+        color_mode="rgb",
+        batch_size=32,
+        class_mode=None,
+        shuffle=False
+    )
+    reference_vecs = model.predict_generator(reference_generator, steps=len(reference_generator),
+                                                                verbose=1)
+
+    return queries, query_vecs, db, reference_vecs
